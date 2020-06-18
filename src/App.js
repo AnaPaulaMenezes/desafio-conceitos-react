ff0ba9b20caa5604f5224d4eff22f9f5542d47cf
Ana Paula Menezes <anapaularossi.577@gmail.com>
2020-06-18 11:29:16 -0300

    frontend

--- src/App.js
+++ src/App.js
@@ -1,30 +1,93 @@
-import React from "react";
-
+import React ,{useState, useEffect} from "react";
+import api from './services/api';
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
+  const [repositories, setRepositories] = useState([]);
+  const [formData, setFormData] = useState({
+    title: '',
+    url: '',
+    techs: [''], 
+  });
+  
+  useEffect(()=>{
+    api.get('repositories').then(response =>{
+      setRepositories(response.data);
+      });
+  },[])
+
+  function handleInputChange(event){
+    const {name, value} = event.target;
+    if (name === "techs"){
+        const techsArray = value.split(',');
+        setFormData({...formData, [name]:techsArray});
+    }else{
+      setFormData({...formData, [name]:value});
+    }
+    
+  } 
+  
+  async function handleAddRepository(event) {
+
+    event.preventDefault();
+        
+    const  {title, url,techs} = formData;
+
+    const data = {};
+    data.title = title;
+    data.url = url;
+    data.techs = [techs];
+
+    const response = await api.post('repositories', data);
+  
+    if (response.status === 200){
+      setRepositories([...repositories,response.data]);
+    }
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete(`repositories/${id}`);
+    
+    if (response.status===204){
+        const repositorieIndex = repositories.findIndex(repo => repo.id === id);
+        const newRepositories = [...repositories]
+        newRepositories.splice(repositorieIndex,1);
+        setRepositories(newRepositories);
+      
+
+    }else if (response.status === 400){
+        alert('Repositorie not found');
+    }
+    
   }
 
   return (
+    <>
+    <div>
+      <h3>Novo repositório</h3><br/>
+      <form onSubmit={handleAddRepository}>
+        <label>Título</label>
+        <input id="title" name="title" onChange={handleInputChange} type="text" />
+        <label>URL</label>
+        <input id="url" name="url" onChange={handleInputChange} type="text"/>
+        <label>Techs</label>
+        <input id="techs" name="techs" onChange={handleInputChange} type="text"/>
+        <button type={"submit"}>Adicionar</button>
+      </form>
+    </div>
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+      {repositories.map(repositorie => (
+      <li key={repositorie.id}>
+          {repositorie.title}
+          <button onClick={() => handleRemoveRepository(repositorie.id)}>
             Remover
           </button>
-        </li>
+        </li>))}
+        
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
+    </>
   );
 }
 
